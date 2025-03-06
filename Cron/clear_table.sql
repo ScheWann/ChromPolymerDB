f9db8fd59a9a75ed1531911d473dcc11278ed613
ScheWann <92758590+ScheWann@users.noreply.github.com>
2025-03-06 00:00:12 -0600

    Update clear_table.sql

--- Cron/clear_table.sql
+++ Cron/clear_table.sql
@@ -1,17 +1,5 @@
 -- clear_tables.sql
-SELECT pg_try_advisory_lock(12345) AS got_lock;
-
-DO $$ 
-DECLARE 
-    lock_status BOOLEAN;
-BEGIN
-    SELECT pg_try_advisory_lock(12345) INTO lock_status;
-    IF lock_status THEN
-        TRUNCATE TABLE position;
-        TRUNCATE TABLE distance;
-
-        PERFORM pg_advisory_unlock(12345);
-    ELSE
-        RAISE NOTICE 'Skipping delete, table is busy';
-    END IF;
-END $$;
+BEGIN;
+LOCK TABLE position, distance IN SHARE MODE NOWAIT;
+TRUNCATE position, distance;
+COMMIT;
