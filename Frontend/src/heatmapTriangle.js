ddc9de80602d32ab6d830daf53e4cec524fa4b6f
ScheWann <92758590+ScheWann@users.noreply.github.com>
2025-04-03 17:11:54 -0500

    changed the triangle opacity

--- Frontend/src/heatmapTriangle.js
+++ Frontend/src/heatmapTriangle.js
@@ -884,7 +884,7 @@ export const HeatmapTriangle = ({ cellLineName, chromosomeName, geneName, curren
                     .attr('class', 'triangle')
                     .attr('points', trianglePoints.map(d => d.join(',')).join(' '))
                     .attr('fill', '#808080')
-                    .attr('opacity', 0.3)
+                    .attr('opacity', 0.1)
 
                 updateAxisWithBrushRange(brushedTriangleRangeStart, brushedTriangleRangeEnd);
                 setBrushedTriangleRange({ start: brushedTriangleRangeStart, end: brushedTriangleRangeEnd });
